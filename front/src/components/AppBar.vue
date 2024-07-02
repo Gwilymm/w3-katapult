1a4a28e0459637e7d5bdca87c7986e33d0d5b8e2
Antoine Bauchot <bchtantoine@gmail.com>
2024-07-02 14:58:20 +0200

    home, navBar, footer, profile, signin WIP

---
+++ front/src/components/AppBar.vue
@@ -0,0 +1,41 @@
+<template>
+<v-app-bar :elevation="5">
+    <div class="">
+        <v-app-bar-nav-icon @click="goToHome"></v-app-bar-nav-icon>
+        <v-app-bar-title>ADRESS Normandie Incabateur Katapult</v-app-bar-title>
+    </div>
+    <template v-slot:append>
+        <v-btn v-if="!isUserConnected" @click="signIn">Sign In</v-btn>
+        <v-btn v-else @click="goToProfile"  icon="mdi-account"></v-btn>
+    </template>
+  </v-app-bar>
+  </template>
+
+  <script setup>
+  import { ref } from 'vue'
+  import { useRouter } from 'vue-router'
+  const isUserConnected = ref(false)
+  const router = useRouter()
+
+  function signIn() {
+    console.log('Sign in')
+    isUserConnected.value = true
+  }
+
+  function goToProfile() {
+    router.push('/profile')
+  }
+
+  function goToHome() {
+    router.push('/')
+  }
+
+  function goToSignIn() {
+    router.push('/signin')
+  }
+  </script>
+
+  <style>
+
+  
+  </style>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -7,6 +7,19 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
+import profile from '../pages/profile.vue'
+import signin from '@/pages/signin.vue'
+
+const routes = [
+  {
+    path: '/profile',
+    component: profile
+  },
+  {
+    path: '/signin',
+    component: signin
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
