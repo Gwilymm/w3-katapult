c9602292fd6da21bc87cb2478341da04f9386663
Gwilymm <marin.davies@gmail.com>
2024-07-02 14:42:50 +0200

    chore: Add mutation and update routes to app.js

--- back/app.js
+++ back/app.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mutationRoutes = require('./routes/mutation');
+const updateRoutes = require('./routes/update');
 const app = express();
 const port = 3000;
 
 // Middleware pour parser les requêtes POST
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Route pour afficher le formulaire
-app.get('../front', (req, res) => {
-	res.render('index');
-});
-
-// Route pour gérer la soumission du formulaire
-app.post('/submit', (req, res) => {
-	const { name, email, message } = req.body;
-	res.send(`Formulaire soumis avec succès !<br>Nom: ${name}<br>Email: ${email}<br>Message: ${message}`);
-});
+// Routes
+app.use('/api/mutation', mutationRoutes);
+app.use('/api/update', updateRoutes);
 
 // Démarrage du serveur
 app.listen(port, () => {
