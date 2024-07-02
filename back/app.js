ff821efb9c2d4bf1f33307d888aae33502aaca43
donne12 <donnesiaku@yahoo.com>
2024-07-02 14:53:00 +0200

    authentification with JWT

--- back/app.js
+++ back/app.js
@@ -6,6 +6,29 @@ const path = require('path');
 const app = express();
 const port = 3001;
 const sequelize = require("./config/database");
+const app = express();
+const port = 3001;
+const { ApolloServer, gql } = require("apollo-server-express");
+const sequelize = require("./config/database");
+const User = require("./models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const secretKey = "katapult_secret_key";
+const multer = require("multer");
+const path = require("path");
+const { GraphQLUpload } = require("graphql-upload");
+
+// Configuration de multer pour les uploads
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); // Ajouter une timestamp pour éviter les conflits de noms
+  },
+});
+
+const upload = multer({ storage: storage });
 
 // Middleware pour parser les requêtes POST
 app.use(bodyParser.json());
